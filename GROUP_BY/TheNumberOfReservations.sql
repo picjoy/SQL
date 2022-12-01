ac227ea53f8250d3e41f147f115acae5e77c3e11
JOY <piczo617@gmail.com>
2022-12-01 16:04:19 +0900

    Create TheNumberOfReservations.sql

---
+++ GROUP_BY/TheNumberOfReservations.sql
@@ -0,0 +1,9 @@
+SELECT MCDP_CD AS '진료과코드'
+, COUNT(MCDP_CD) AS '5월예약건수'
+FROM APPOINTMENT 
+WHERE 1=1
+AND APNT_YMD LIKE '2022-05%'
+-- LIKE : 특정 문자 또는 문자열을 포함하고 있는 값을 검색하고 싶을 때 사용
+GROUP BY MCDP_CD
+-- GROUP BY : 특정 컬럼을 그룹화
+ORDER BY COUNT(MCDP_CD), MCDP_CD
